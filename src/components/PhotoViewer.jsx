386ff15fc04ee337d917efae514c031926a492e0
sideroad <sideroad.jp@gmail.com>
2017-03-05 18:39:19 +0900

    Hide info when user swiped

--- src/components/PhotoViewer.jsx
+++ src/components/PhotoViewer.jsx
@@ -35,7 +35,8 @@ class PhotoViewer extends Component {
       className: this.state.className === 'leftOut' ? 'rightIn' :
                  this.state.className === 'rightOut' ? 'leftIn' :
                  this.state.className || '',
-      swiping: false
+      swiping: false,
+      info: false,
     });
   }
 
