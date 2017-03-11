ef75d80924148f58f1db0bc8e9306ae5322230f5
sideroad <sideroad.jp@gmail.com>
2017-03-11 21:26:53 +0900

    Update loading icon

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -9,6 +9,10 @@ const Loading = () =>
       <div className={`${styles.circle} ${styles.left}`} />
       <div className={`${styles.circle} ${styles.bottom}`} />
       <div className={`${styles.circle} ${styles.right}`} />
+      <div className={`${styles.circle} ${styles.scale} ${styles.top}`} />
+      <div className={`${styles.circle} ${styles.scale} ${styles.left}`} />
+      <div className={`${styles.circle} ${styles.scale} ${styles.bottom}`} />
+      <div className={`${styles.circle} ${styles.scale} ${styles.right}`} />
       <div className={styles.icon} />
     </div>
   </div>;
