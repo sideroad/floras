cb747c88d83b1988c46d92f7b61d0c311c439a3d
sideroad <sideroad.jp@gmail.com>
2021-03-31 23:45:49 +0900

    Bugfix to display calendar.

--- components/koiki-ui/Calendar.jsx
+++ components/koiki-ui/Calendar.jsx
@@ -1,232 +1,221 @@
-import React, { Component } from 'react';
+import React, { Component, useRef, useState } from 'react';
 import moment from 'moment';
 import __ from 'lodash';
-import { Swipeable } from 'react-swipeable';
+import { useSwipeable } from 'react-swipeable';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Calendar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: moment.utc(this.props.date, 'YYYY-MM-DD'),
-      className: '',
-      swiping: false,
-    };
+const Calendar = (props) => {
+  const [date, setDate] = useState(moment.utc(props.date, 'YYYY-MM-DD'));
+  const [className, setClassName] = useState('');
+  const [swiping, setSwiping] = useState(false);
+  const start = moment
+    .utc(date)
+    .startOf('month')
+    .startOf('week');
+  const end = moment
+    .utc(date)
+    .endOf('month')
+    .endOf('week');
+  const linesOfWeek = (end.diff(start, 'days') + 1) / 7;
+  const isEnablePrevMonth = !props.min ? true : start.isSameOrAfter(props.min);
+  const isEnableNextMonth = !props.max ? true : end.isSameOrBefore(props.max);
+
+  const nextMonth = (swiping) => {
+    setDate(moment.utc(date).add(1, 'months'));
+    setClassName('nextMonth');
+    setSwiping(swiping);
+    setTimeout(() => {
+      setClassName('');
+      setSwiping(false);
+    }, 300);
   }
 
-  nextMonth(swiping) {
-    const that = this;
-    this.setState({
-      date: moment.utc(this.state.date).add(1, 'months'),
-      className: 'nextMonth',
-      swiping,
-    });
-    setTimeout(() => that.setState({ className: '', swiping: false }), 300);
+  const prevMonth = (swiping) => {
+    setDate(moment.utc(date).subtract(1, 'months'));
+    setClassName('prevMonth');
+    setSwiping(swiping);
+    setTimeout(() => {
+      setClassName('');
+      setSwiping(false);
+    }, 300);
   }
 
-  prevMonth(swiping) {
-    const that = this;
-    this.setState({
-      date: moment.utc(this.state.date).subtract(1, 'months'),
-      className: 'prevMonth',
-      swiping,
-    });
-    setTimeout(() => that.setState({ className: '', swiping: false }), 300);
+  const swiped = () => {
+    setSwiping(false);
   }
 
-  swiped() {
-    this.setState({
-      swiping: false,
-    });
-  }
-
-  swipingNext() {
-    if (!this.state.swiping) {
-      this.nextMonth(true);
+  const swipingNext = () => {
+    if (!swiping) {
+      nextMonth(true);
     }
   }
 
-  swipingPrev() {
-    if (!this.state.swiping) {
-      this.prevMonth(true);
+  const swipingPrev = () => {
+    if (!swiping) {
+      prevMonth(true);
     }
   }
 
-  select(evt, _date) {
+  const select = (evt, _date) => {
     evt.preventDefault();
     evt.target.blur();
-    if (_date.isAfter(this.state.date, 'month')) {
-      this.nextMonth(evt);
+    if (_date.isAfter(date, 'month')) {
+      nextMonth(evt);
     }
-    if (_date.isBefore(this.state.date, 'month')) {
-      this.prevMonth(evt);
+    if (_date.isBefore(date, 'month')) {
+      prevMonth(evt);
     }
-    this.props.onSelect(_date.format('YYYY-MM-DD'));
+    props.onSelect(_date.format('YYYY-MM-DD'));
   }
 
-  render() {
-    const start = moment
-      .utc(this.state.date)
-      .startOf('month')
-      .startOf('week');
-    const end = moment
-      .utc(this.state.date)
-      .endOf('month')
-      .endOf('week');
-    const linesOfWeek = (end.diff(start, 'days') + 1) / 7;
-    const isEnablePrevMonth = !this.props.min ? true : start.isSameOrAfter(this.props.min);
-    const isEnableNextMonth = !this.props.max ? true : end.isSameOrBefore(this.props.max);
+  const handlers = useSwipeable({
+    onSwipingRight: swipingPrev,
+    onSwipedRight: swiped,
+    onSwipingLeft: swipingNext,
+    onSwipedLeft: swiped
+  });
 
-    return (
-      <Swipeable
-        className={`${this.props.styles.calendar.calendar} ${this.props.className}`}
-        onSwipingRight={this.swipingPrev}
-        onSwipedRight={this.swiped}
-        onSwipingLeft={this.swipingNext}
-        onSwipedLeft={this.swiped}
-      >
-        <div
-          ref={(elem) => {
-            this.calendarDOM = elem;
-          }}
-        >
-          <div className={`${this.props.styles.calendar.control}`}>
-            <div className={this.props.styles.calendar.prev}>
-              {isEnablePrevMonth ? (
-                <a
-                  className={this.props.styles.calendar.link}
-                  onClick={(evt) => {
-                    evt.preventDefault();
-                    this.prevMonth();
-                  }}
-                  href=""
-                >
-                  <div className={this.props.styles.calendar.linkcircle} />
-                  <span>
-                    <i className="fa fa-chevron-left" aria-hidden="true" />
-                  </span>
-                </a>
-              ) : null}
-            </div>
-            <div
-              className={`${this.props.styles.calendar.month} ${
-                this.props.styles.calendar[this.state.className]
-              }`}
+  return (
+    <div
+      {...handlers}
+      className={`${props.styles.calendar.calendar} ${props.className}`}
+      ref={props.innerRef}
+    >
+      <div className={`${props.styles.calendar.control}`}>
+        <div className={props.styles.calendar.prev}>
+          {isEnablePrevMonth ? (
+            <a
+              className={props.styles.calendar.link}
+              onClick={(evt) => {
+                evt.preventDefault();
+                prevMonth();
+              }}
+              href=""
             >
-              {`${this.state.date.format('MMMM')} ${this.state.date.format('YYYY')}`}
-            </div>
-            <div className={this.props.styles.calendar.next}>
-              {isEnableNextMonth ? (
-                <a
-                  className={this.props.styles.calendar.link}
-                  onClick={(evt) => {
-                    evt.preventDefault();
-                    this.nextMonth();
-                  }}
-                  href=""
-                >
-                  <div className={this.props.styles.calendar.linkcircle} />
-                  <span>
-                    <i className="fa fa-chevron-right" aria-hidden="true" />
-                  </span>
-                </a>
-              ) : null}
-            </div>
-          </div>
-          <table
-            className={`${this.props.styles.calendar.table} ${
-              this.props.styles.calendar[this.state.className]
-            }`}
-          >
-            <thead>
-              <tr>
-                {__.times(7, index => (
-                  <th key={index} className={this.props.styles.calendar.weekday}>
-                    {moment()
-                      .weekday(index)
-                      .format('ddd')}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {__.times(linesOfWeek, week => (
-                <tr key={week}>
-                  {__.times(7, (weekday) => {
-                    const dateOfWeekday = moment.utc(start).add(week * 7 + weekday, 'days');
-                    const timeOfWeekday = dateOfWeekday.toDate().getTime();
-                    const dateClassName = [
-                      __.some(this.props.selected, item =>
-                        dateOfWeekday.isSame(
-                          moment.utc(__.isString(item) ? item : item.date).startOf('date')
-                        )
-                      )
-                        ? this.props.styles.calendar.selected
-                        : '',
-                      dateOfWeekday.isSame(
-                        moment.utc(this.props.today, 'YYYY-MM-DD').startOf('date')
-                      )
-                        ? this.props.styles.calendar.today
-                        : __.some(this.props.holidays, item =>
-                            dateOfWeekday.isSame(moment.utc(item).startOf('date'))
-                          )
-                        ? this.props.styles.calendar.holiday
-                        : !dateOfWeekday.isSame(this.state.date, 'month')
-                        ? this.props.styles.calendar.outside
-                        : '',
-                      this.props.styles.calendar[dateOfWeekday.format('ddd').toLowerCase()],
-                    ].join(' ');
-                    const selected =
-                      __.find(
-                        this.props.selected,
-                        item =>
-                          !__.isString(item) &&
-                          dateOfWeekday.isSame(moment.utc(item.date).startOf('date'))
-                      ) || {};
-                    return (
-                      <td key={timeOfWeekday} className={this.props.styles.calendar.col}>
-                        <div className={dateClassName}>
-                          <div className={this.props.styles.calendar.date}>
-                            {(this.props.min &&
-                              moment
-                                .utc(this.props.min, 'YYYY-MM-DD')
-                                .toDate()
-                                .getTime() > timeOfWeekday) ||
-                            (this.props.max &&
-                              moment
-                                .utc(this.props.max, 'YYYY-MM-DD')
-                                .toDate()
-                                .getTime() < timeOfWeekday) ? (
-                                  <div className={this.props.styles.calendar.disabled}>
-                                    <span>{dateOfWeekday.date()}</span>
-                                  </div>
-                            ) : (
-                              <a
-                                className={this.props.styles.calendar.link}
-                                href=""
-                                onClick={evt => this.select(evt, dateOfWeekday)}
-                              >
-                                <div
-                                  className={this.props.styles.calendar.linkcircle}
-                                  style={selected.style || {}}
-                                />
-                                <span>{dateOfWeekday.date()}</span>
-                              </a>
-                            )}
-                          </div>
-                        </div>
-                      </td>
-                    );
-                  })}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+              <div className={props.styles.calendar.linkcircle} />
+              <span>
+                <i className="fa fa-chevron-left" aria-hidden="true" />
+              </span>
+            </a>
+          ) : null}
         </div>
-      </Swipeable>
-    );
-  }
-}
+        <div
+          className={`${props.styles.calendar.month} ${
+            props.styles.calendar[className]
+          }`}
+        >
+          {`${date.format('MMMM')} ${date.format('YYYY')}`}
+        </div>
+        <div className={props.styles.calendar.next}>
+          {isEnableNextMonth ? (
+            <a
+              className={props.styles.calendar.link}
+              onClick={(evt) => {
+                evt.preventDefault();
+                nextMonth();
+              }}
+              href=""
+            >
+              <div className={props.styles.calendar.linkcircle} />
+              <span>
+                <i className="fa fa-chevron-right" aria-hidden="true" />
+              </span>
+            </a>
+          ) : null}
+        </div>
+      </div>
+      <table
+        className={`${props.styles.calendar.table} ${
+          props.styles.calendar[className]
+        }`}
+      >
+        <thead>
+          <tr>
+            {__.times(7, index => (
+              <th key={index} className={props.styles.calendar.weekday}>
+                {moment()
+                  .weekday(index)
+                  .format('ddd')}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {__.times(linesOfWeek, week => (
+            <tr key={week}>
+              {__.times(7, (weekday) => {
+                const dateOfWeekday = moment.utc(start).add(week * 7 + weekday, 'days');
+                const timeOfWeekday = dateOfWeekday.toDate().getTime();
+                const dateClassName = [
+                  __.some(props.selected, item =>
+                    dateOfWeekday.isSame(
+                      moment.utc(__.isString(item) ? item : item.date).startOf('date')
+                    )
+                  )
+                    ? props.styles.calendar.selected
+                    : '',
+                  dateOfWeekday.isSame(
+                    moment.utc(props.today, 'YYYY-MM-DD').startOf('date')
+                  )
+                    ? props.styles.calendar.today
+                    : __.some(props.holidays, item =>
+                        dateOfWeekday.isSame(moment.utc(item).startOf('date'))
+                      )
+                    ? props.styles.calendar.holiday
+                    : !dateOfWeekday.isSame(date, 'month')
+                    ? props.styles.calendar.outside
+                    : '',
+                  props.styles.calendar[dateOfWeekday.format('ddd').toLowerCase()],
+                ].join(' ');
+                const selected =
+                  __.find(
+                    props.selected,
+                    item =>
+                      !__.isString(item) &&
+                      dateOfWeekday.isSame(moment.utc(item.date).startOf('date'))
+                  ) || {};
+                return (
+                  <td key={timeOfWeekday} className={props.styles.calendar.col}>
+                    <div className={dateClassName}>
+                      <div className={props.styles.calendar.date}>
+                        {(props.min &&
+                          moment
+                            .utc(props.min, 'YYYY-MM-DD')
+                            .toDate()
+                            .getTime() > timeOfWeekday) ||
+                        (props.max &&
+                          moment
+                            .utc(props.max, 'YYYY-MM-DD')
+                            .toDate()
+                            .getTime() < timeOfWeekday) ? (
+                              <div className={props.styles.calendar.disabled}>
+                                <span>{dateOfWeekday.date()}</span>
+                              </div>
+                        ) : (
+                          <a
+                            className={props.styles.calendar.link}
+                            href=""
+                            onClick={evt => select(evt, dateOfWeekday)}
+                          >
+                            <div
+                              className={props.styles.calendar.linkcircle}
+                              style={selected.style || {}}
+                            />
+                            <span>{dateOfWeekday.date()}</span>
+                          </a>
+                        )}
+                      </div>
+                    </div>
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 Calendar.defaultProps = {
   className: '',
--- components/koiki-ui/Datepicker.jsx
+++ components/koiki-ui/Datepicker.jsx
@@ -1,110 +1,88 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import TetherComponent from 'react-tether';
 import _ from 'lodash';
 import Calendar from './Calendar';
 import format from './dates-format';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Datepicker extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      opened: false,
-      selected: props.selected,
-    };
-  }
-  componentDidMount() {
-    this.wrappedHandleClickOutside = evt =>
-      this.handleClickOutside(evt, this.containerDOM, this.calendar);
-    document.addEventListener('click', this.wrappedHandleClickOutside, true);
-  }
+const Datepicker = (props) => {
+  const ref = useRef(null);
+  const calendarRef = useRef(null);
+  const [opened, setOpened] = useState(false);
+  const [selected, setSelected] = useState(props.selected);
 
-  componentWillUnmount() {
-    document.removeEventListener('click', this.wrappedHandleClickOutside, true);
-  }
-
-  handleClickOutside(evt, containerDOM, calendar) {
+  const handleClickOutside = (evt, ref, calendarRef) => {
     if (
-      (!containerDOM || !containerDOM.contains(evt.target)) &&
-      (!calendar || !calendar.calendarDOM || !calendar.calendarDOM.contains(evt.target))
+      (!ref || !ref.contains(evt.target)) &&
+      (!calendarRef || !calendarRef.contains(evt.target))
     ) {
-      this.setState({
-        opened: false,
-      });
+      setOpened(false);
     }
-  }
+  };
+  useEffect(() => {
+    const wrappedHandleClickOutside = evt => handleClickOutside(evt, ref, calendarRef);
+    document.addEventListener('click', wrappedHandleClickOutside, true);
+    return () => document.removeEventListener('click', wrappedHandleClickOutside, true);
+  }, []);
 
-  render() {
-    return (
-      <div className={this.props.className}>
-        <TetherComponent
-          classes={{
-            element: this.props.styles.datepicker.tether,
-          }}
-          attachment="top left"
-          targetAttachment="bottom left"
+  return (
+    <div className={props.className}>
+      <TetherComponent
+        classes={{
+          element: props.styles.datepicker.tether,
+        }}
+        attachment="top left"
+        targetAttachment="bottom left"
+      >
+        <div
+          ref={ref}
+          className={props.styles.datepicker.container}
         >
-          <div
-            ref={(elem) => {
-              this.containerDOM = elem;
+          <button
+            className={props.styles.datepicker.button}
+            onClick={() => {
+              setOpened(!opened);
+              props.onClick();
             }}
-            className={this.props.styles.datepicker.container}
           >
-            <button
-              className={this.props.styles.datepicker.button}
-              onClick={() => {
-                this.setState({
-                  opened: !this.state.opened,
-                });
-                this.props.onClick();
-              }}
-            >
-              <i className={`fa ${this.props.icon}`} aria-hidden="true" />
-              <span
-                className={
-                  this.state.selected.length
-                    ? this.props.styles.datepicker.date
-                    : this.props.styles.datepicker.placeholder
-                }
-              >
-                {format({
-                  dates: this.state.selected.sort(),
-                  format: this.props.format,
-                  delimiter: this.props.delimiter,
-                  range: this.props.range,
-                }) || this.props.placeholder}
-              </span>
-            </button>
-          </div>
-          <Calendar
-            styles={this.props.styles}
-            ref={(elem) => {
-              this.calendar = elem;
-            }}
-            className={`
-              ${this.props.styles.datepicker.calendar}
-              ${
-                this.state.opened
-                  ? this.props.styles.datepicker.opened
-                  : this.props.styles.datepicker.closed
-              }`}
-            selected={this.state.selected}
-            onSelect={(date) => {
-              if (this.state.selected.indexOf(date) === -1) {
-                this.setState({
-                  selected: this.state.selected.concat([date]),
-                });
-              } else {
-                this.setState({
-                  selected: _.pull(this.state.selected, date),
-                });
+            <i className={`fa ${props.icon}`} aria-hidden="true" />
+            <span
+              className={
+                selected.length
+                  ? props.styles.datepicker.date
+                  : props.styles.datepicker.placeholder
               }
-            }}
-          />
-        </TetherComponent>
-      </div>
-    );
-  }
+            >
+              {format({
+                dates: selected.sort(),
+                format: props.format,
+                delimiter: props.delimiter,
+                range: props.range,
+              }) || props.placeholder}
+            </span>
+          </button>
+        </div>
+        <Calendar
+          styles={props.styles}
+          innerRef={calendarRef}
+          className={`
+            ${props.styles.datepicker.calendar}
+            ${
+              opened
+                ? props.styles.datepicker.opened
+                : props.styles.datepicker.closed
+            }`}
+          selected={selected}
+          onSelect={(date) => {
+            if (selected.indexOf(date) === -1) {
+              setSelected(selected.concat([date]))
+            } else {
+              setSelected(_.pull(selected, date));
+            }
+          }}
+        />
+      </TetherComponent>
+    </div>
+  );
 }
 
 Datepicker.defaultProps = {
